fc1ae26890469ce8641314c036169feb303169f1
ali.husnain.010203 <ali.husnain.010203@gmail.com>
2024-10-25 11:41:33 +0000

    start-screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,27 @@
 import { Stack } from "expo-router";
-
+import { useFonts } from "expo-font";
 export default function RootLayout() {
+  
+  useFonts({
+    "outline":require("../assets/fonts/Outfit-Regular.ttf"),
+    "outline-bold":require("../assets/fonts/Outfit-Bold.ttf"),
+    "outline-black":require("../assets/fonts/Outfit-Black.ttf"),
+    "outline-extra-bold":require("../assets/fonts/Outfit-ExtraBold.ttf"),
+    "outline-extra-light":require("../assets/fonts/Outfit-ExtraLight.ttf"),
+    "outline-light":require("../assets/fonts/Outfit-Light.ttf"),
+    "outline-medium":require("../assets/fonts/Outfit-Medium.ttf"),
+    "outline-semi-bold":require("../assets/fonts/Outfit-SemiBold.ttf"),
+    "outline-thin":require("../assets/fonts/Outfit-Thin.ttf"),
+
+  })
   return (
     <Stack>
       <Stack.Screen name="index" />
+      <Stack.Screen name="login/index"
+      options={{
+        headerShown:false
+      }}
+      />
     </Stack>
   );
 }
---
+++ app/login/index.tsx
@@ -0,0 +1,55 @@
+import { View, Text, Image, Pressable } from 'react-native'
+import React from 'react'
+import Colors from "../../constants/color"
+import {Dimensions} from "react-native"
+
+export default function index() {
+  const { height } = Dimensions.get('window');
+  return (
+    <View>
+      <Image source={require("../../assets/images/loginImage.png")}
+        style={{
+          width: "100%",
+          height:  height * 0.6 ,
+         
+        }} />
+      <View style={{
+        padding: 20,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+      }}>
+        <Text style={{
+          fontFamily: 'outline-bold',
+          fontSize: 30,
+          textAlign: "center"
+        }}>Ready to make a new friend ?</Text>
+        <Text style={{
+          fontFamily: "outline",
+          fontSize: 18,
+          textAlign: "center",
+          color: Colors.GRAY
+        }}>
+          Let's adopt the pet which you like ...🐶
+        </Text>
+        <Pressable style={{
+          padding: 14,
+          margin: 50,
+          backgroundColor: Colors.PRIMARY,
+          borderRadius: 10,
+          display: "flex",
+          width:"100%",
+          justifyContent: "center",
+          alignItems: "center"
+        }}>
+          <Text
+            style={{
+              fontFamily: "outline-bold",
+              fontSize: 18,
+              color: Colors.WHITE,
+            }}>Get Started</Text>
+        </Pressable>
+      </View>
+    </View>
+  )
+}
