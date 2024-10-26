49cc87bdbb1fabbe3cd8d97ed310256d5916b7df
ali.husnain.010203 <ali.husnain.010203@gmail.com>
2024-10-26 05:54:53 +0000

    Authentication

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,9 @@
-import { Stack } from "expo-router";
-import { useFonts } from "expo-font";
+import * as SecureStore from 'expo-secure-store'
+import { ClerkProvider, ClerkLoaded } from '@clerk/clerk-expo'
+import { Slot, Stack } from 'expo-router'
+import { useFonts } from 'expo-font'
+
 export default function RootLayout() {
-  
   useFonts({
     "outline":require("../assets/fonts/Outfit-Regular.ttf"),
     "outline-bold":require("../assets/fonts/Outfit-Bold.ttf"),
@@ -14,14 +16,56 @@ export default function RootLayout() {
     "outline-thin":require("../assets/fonts/Outfit-Thin.ttf"),
 
   })
+  const tokenCache = {
+    async getToken(key: string) {
+      try {
+        const item = await SecureStore.getItemAsync(key)
+        if (item) {
+          console.log(`${key} was used 🔐 \n`)
+        } else {
+          console.log('No values stored under key: ' + key)
+        }
+        return item
+      } catch (error) {
+        console.error('SecureStore get item error: ', error)
+        await SecureStore.deleteItemAsync(key)
+        return null
+      }
+    },
+    async saveToken(key: string, value: string) {
+      try {
+        return SecureStore.setItemAsync(key, value)
+      } catch (err) {
+        return
+      }
+    },
+  }
+
+  const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+
+  if (!publishableKey) {
+    throw new Error(
+      'Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env',
+    )
+  }
+
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-      <Stack.Screen name="login/index"
-      options={{
+    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
+      <ClerkLoaded>
+      <Stack>
+      <Stack.Screen name="index" options={{
+        headerShown: false,
+      }} />
+      <Stack.Screen name="(auth)/sign-in" options={{
         headerShown:false
-      }}
-      />
+      }}/>
+      
+      <Stack.Screen name="(auth)/sign-up" options={{
+        headerShown:false
+      }}/>
+     
     </Stack>
-  );
+      </ClerkLoaded>
+    </ClerkProvider>
+  )
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,58 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { View, Text, Image, Pressable } from 'react-native'
+import React from 'react'
+import Colors from "../constants/color"
+import {Dimensions} from "react-native"
+import { Link } from 'expo-router';
 
-export default function Index() {
+export default function index() {
+  const { height } = Dimensions.get('window');
   return (
-    <View
-      style={{
-        flex: 1,
+    <View>
+      <Image source={require("../assets/images/loginImage.png")}
+        style={{
+          width: "100%",
+          height:  height * 0.6 ,
+         
+        }} />
+      <View style={{
+        padding: 20,
+        display: "flex",
         justifyContent: "center",
-        alignItems: "center",
-
-      }}
-    >
-   <Link href={"/login"}>
-   <Text>Go to Login</Text>
-   </Link>
+        alignItems: "center"
+      }}>
+        <Text style={{
+          fontFamily: 'outline-bold',
+          fontSize: 30,
+          textAlign: "center"
+        }}>Ready to make a new friend ?</Text>
+        <Text style={{
+          fontFamily: "outline",
+          fontSize: 18,
+          textAlign: "center",
+          color: Colors.GRAY
+        }}>
+          Let's adopt the pet which you like ...🐶
+        </Text>
+        <Pressable style={{
+          padding: 14,
+          margin: 50,
+          backgroundColor: Colors.PRIMARY,
+          borderRadius: 10,
+          display: "flex",
+          width:"100%",
+          justifyContent: "center",
+          alignItems: "center"
+        }}>
+          <Link href="/(auth)/sign-up">
+          <Text
+            style={{
+              fontFamily: "outline-bold",
+              fontSize: 18,
+              color: Colors.WHITE,
+            }}>Get Started</Text>
+            </Link>
+        </Pressable>
+      </View>
     </View>
-  );
+  )
 }
