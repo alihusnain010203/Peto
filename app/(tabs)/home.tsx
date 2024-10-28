6862333e1e664d1a039e2ac7737f16025d84eb09
ali.husnain.010203 <ali.husnain.010203@gmail.com>
2024-10-28 10:55:25 +0000

    start-home

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,10 +1,44 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
+import { useUser } from '@clerk/clerk-expo'
+import Colors from "../../constants/color"
 
-export default function home() {
+export default function Home() {
+  const { user } = useUser();
+
   return (
     <View>
-      <Text>home</Text>
+      <View 
+        style={{
+          display: "flex",
+          flexDirection:"row",
+          justifyContent: "space-between",
+          alignItems: 'center',
+          padding: 20
+        }}
+      >
+        <Text style={{
+          color: Colors.PRIMARY,
+          fontFamily: "outline-semibold",
+          fontSize:20
+        }}>
+          Hello {user?.firstName || 'Guest'}
+        </Text>
+        
+        {user?.imageUrl ? (
+          <Image 
+            source={{ uri: user.imageUrl }} 
+            alt='User Image' 
+            style={{
+              width: 25,
+              height: 25,
+              borderRadius: 50,
+              marginTop: 10
+            }} 
+          />
+        ) : null}
+        
+      </View>
     </View>
   )
 }
